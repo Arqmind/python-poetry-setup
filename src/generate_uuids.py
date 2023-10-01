5a312cd80f3bffcf2863e22153cebb9eb3011a1b
bchathoth-wt <bijeesh.chathoth@gmail.com>
2023-10-01 13:31:29 +0100

    Added docstrings

--- src/generate_uuids.py
+++ src/generate_uuids.py
@@ -4,6 +4,7 @@ import uuid
 def generate_random_uuid():
     """
     Generate random uuid
+    @return: uuid
     """
     # Generate a random UUID (version 4)
     random_uuid = uuid.uuid4()
@@ -12,6 +13,11 @@ def generate_random_uuid():
 
 
 def generate_time_based_uuid():
+    """
+    geterating time base uuid
+
+    @return: uuid
+    """
     # Generate a UUID based on the host's MAC address and the current time (version 1)
     time_based_uuid = uuid.uuid1()
     print("Time-based UUID (version 1):", time_based_uuid)
@@ -19,6 +25,14 @@ def generate_time_based_uuid():
 
 
 def generate_namespace_based_uuid(namespace, name):
+    """
+    Generate namespace based uuid
+
+    @param namespace:str
+    @name str
+
+    @return: uuid
+    """
     # Generate a UUID based on a namespace and a name (version 5)
     # namespace = uuid.UUID("6ba7b810-9dad-11d1-80b4-00c04fd430c8")
     # name = "example.com"
--- test/test_uuid_generation.py
+++ test/test_uuid_generation.py
@@ -3,16 +3,25 @@ from src import generate_uuids as gu
 
 
 def test_generate_random_uuid():
+    """
+    Unit testing - generate random uuid
+    """
     random_uuid = gu.generate_random_uuid()
     assert uuid.UUID(random_uuid.hex).version == 4
 
 
 def test_generate_time_based_uuid():
+    """
+    Unit testing - generate time based uuid
+    """
     time_based_uuid = gu.generate_time_based_uuid()
     assert uuid.UUID(time_based_uuid.hex).version == 1
 
 
 def test_generate_namespace_based_uuid():
+    """
+    Unit testing - generate namespace based uuid
+    """
     namespace = uuid.UUID("6ba7b810-9dad-11d1-80b4-00c04fd430c8")
     name = "example.com"
     namespace_based_uuid = gu.generate_namespace_based_uuid(namespace, name)
